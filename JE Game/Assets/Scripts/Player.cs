c83d5ae988d7fe4ec4d4d1d10894b6d064fc2463
Laheau <laheau@users.noreply.github.com>
2018-10-17 20:38:17 -0400

    Test of movement script

--- JE Game/Assets/Scripts/Player.cs
+++ JE Game/Assets/Scripts/Player.cs
@@ -4,13 +4,33 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
+    GameObject player;
+    float speed = 0.1f;
+    Rigidbody2D rb;
+
 	// Use this for initialization
 	void Start () {
-		
+        player = this.gameObject;
+        rb = player.GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        if (Input.GetKey(KeyCode.W))
+        {
+            player.transform.Translate(player.transform.up * speed);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            player.transform.Translate(player.transform.right * speed);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            player.transform.Translate(-player.transform.right * speed);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            player.transform.Translate(-player.transform.up * speed);
+        }
+    }
 }
